472e559dd55c6050c8daec2cd9dba498a481b0f6
fhramosm406-afk <fhramosm406@gmail.com>
2026-09-05 15:02:29 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,57 +1,37 @@
-const CACHE_NAME = "post-sismo-v2";
+const CACHE_NAME = "post-sismo-v3";
 
 const ARCHIVOS = [
   "./",
   "./index.html",
+  "./GPSOFFLINE.html",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ARCHIVOS))
+      .then(() => self.skipWaiting())
   );
-
-  self.skipWaiting();
-
 });
-
 
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-
-    caches.keys().then(keys =>
-
-      Promise.all(
-
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-
-      )
-
-    )
-
+    caches.keys().then(nombres => {
+      return Promise.all(
+        nombres
+          .filter(nombre => nombre !== CACHE_NAME)
+          .map(nombre => caches.delete(nombre))
+      );
+    }).then(() => self.clients.claim())
   );
-
-  self.clients.claim();
-
 });
 
-
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
     caches.match(event.request)
-      .then(response => {
-
-        return response || fetch(event.request);
-
+      .then(respuesta => {
+        return respuesta || fetch(event.request);
       })
-
   );
-
 });
